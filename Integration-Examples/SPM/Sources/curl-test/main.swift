2dedaa5c1d22301df07c48b9e1e192b7f2faa9ce
Matteo Carrara <matte.carra@gmail.com>
2022-04-03 12:15:25 +0200

    Working version

--- Integration-Examples/SPM/Sources/curl-test/main.swift
+++
@@ -1,3 +0,0 @@
-import curl
-
-print("Hello, world!")
--- scripts/create-frameworks.sh
+++ scripts/create-frameworks.sh
@@ -6,54 +6,14 @@ set -e
 BASE_PWD="$PWD"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 FWNAME="curl"
-OUTPUT_DIR=$( mktemp -d )
-COMMON_SETUP=" -project ${SCRIPT_DIR}/../${FWNAME}.xcodeproj -configuration Release -quiet BUILD_LIBRARY_FOR_DISTRIBUTION=YES "
-
-# iOS
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-    -scheme "${FWNAME} (iOS)" \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=iOS'
-
-rm -rf "${OUTPUT_DIR}/iphoneos"
-mkdir -p "${OUTPUT_DIR}/iphoneos"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-iphoneos/${FWNAME}.framework" "${OUTPUT_DIR}/iphoneos/${FWNAME}.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
-# iOS Simulator
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-    -scheme "${FWNAME} (iOS Simulator)" \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=iOS Simulator'
-
-rm -rf "${OUTPUT_DIR}/iphonesimulator"
-mkdir -p "${OUTPUT_DIR}/iphonesimulator"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-iphonesimulator/${FWNAME}.framework" "${OUTPUT_DIR}/iphonesimulator/${FWNAME}.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
-#
-
-rm -rf "${BASE_PWD}/Frameworks/iphoneos"
-mkdir -p "${BASE_PWD}/Frameworks/iphoneos"
-ditto "${OUTPUT_DIR}/iphoneos/${FWNAME}.framework" "${BASE_PWD}/Frameworks/iphoneos/${FWNAME}.framework"
-
-rm -rf "${BASE_PWD}/Frameworks/iphonesimulator"
-mkdir -p "${BASE_PWD}/Frameworks/iphonesimulator"
-ditto "${OUTPUT_DIR}/iphonesimulator/${FWNAME}.framework" "${BASE_PWD}/Frameworks/iphonesimulator/${FWNAME}.framework"
 
 # XCFramework
 rm -rf "${BASE_PWD}/Frameworks/${FWNAME}.xcframework"
 
-xcrun xcodebuild -quiet -create-xcframework \
-	-framework "${OUTPUT_DIR}/iphoneos/${FWNAME}.framework" \
-	-framework "${OUTPUT_DIR}/iphonesimulator/${FWNAME}.framework" \
+xcrun xcodebuild -create-xcframework \
+	-library "${BASE_PWD}/iphonesimulator/lib/libcurl.a" -headers "${BASE_PWD}/iphonesimulator/include" \
+	-library "${BASE_PWD}/iphoneos/lib/libcurl.a" -headers "${BASE_PWD}/iphoneos/include" \
 	-output "${BASE_PWD}/Frameworks/${FWNAME}.xcframework"
 
 # Zip archive
 zip --symlinks -r "${BASE_PWD}/Frameworks/${FWNAME}.xcframework.zip" "${BASE_PWD}/Frameworks/${FWNAME}.xcframework"
-
-rm -rf ${OUTPUT_DIR}
